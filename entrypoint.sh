fd320b11cfe55ce300ce2fcff3484a75f1778947
Mafe Avendaño <mafe.ud@gmail.com>
2018-04-27 11:20:39 -0500

    Se agregan archivos de configuración CI

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+if [ -n "${PARAMETER_STORE:-}" ]; then
+  export FORMACION_ACADEMICA_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/formacion_academica_crud/db/username --output text --query Parameter.Value)"
+  export FORMACION_ACADEMICA_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/formacion_academica_crud/db/password --output text --query Parameter.Value)"
+fi
+
+exec ./main "$@"
